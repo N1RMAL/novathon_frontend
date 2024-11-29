c2cfaf027b76d9ca52048cca3fecf298111e1081
Sukruth K <sukruthk17@gmail.com>
2024-11-29 07:41:42 +0530

    reg,file

--- case-files-search/src/CaseFilesSearch.jsx
+++ case-files-search/src/CaseFilesSearch.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import SummaryDialogue from './CaseSummaryModal';
+import DisplayPDF from './displaypdf';
 
 const CaseFilesSearch = () => {
   const [searchParams, setSearchParams] = useState({
